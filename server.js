3576097f105201033540773d6549a0aac0f1ae2f
Erin <erin.vaa@gmail.com>
2018-01-28 19:39:47 -0500

    Now caching data from senpai.moe for episode start times

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const request = require('request');
 
 const app = express();
 
+var startTimeMap = {};
+
 app.listen(8000, () => {
   console.log('Server started!');
+  loadAnimeStartTimes();
 });
 
 app.get('/', (req, res) => res.send('Hello World!'));
@@ -37,23 +40,25 @@ const airedEpisodesKey = 'anime_aired_episodes' // My own creation
 const animeAiringStatus = 'anime_airing_status'
 const animeNumEpisodes = 'anime_num_episodes'
 const animeStartDate = 'anime_start_date_string'
+const animeId = 'anime_id'
 
-// One week in milliseconds
-const oneWeek = 1000*60*60*24*7
+// One week in seconds
+const oneWeek = 60*60*24*7
 
 function addAiredEpisodeCount(data) {
   var anime;
-  var startDate;
+  var startDate, malId;
   var millisecondsAiring;
+  var currentTime = new Date().getTime() / 1000;
   for (var i = 0; i < data.length; i++) {
     anime = data[i];
     if (anime[animeAiringStatus] == 1) {
       // Currently airing anime. Most of logic is done here.
-      startDate = Date.parse(anime[animeStartDate])
-      millisecondsAiring = new Date().getTime() - startDate;
+      // startDate = Date.parse(anime[animeStartDate])
+      malId = anime[animeId];
+      startDate = startTimeMap[malId];
+      millisecondsAiring = currentTime - startDate;
       anime[airedEpisodesKey] = parseInt(millisecondsAiring / oneWeek) + 1;
-      // Note currently this logic doesn't include start time at all; just start date. Eventual plan is to cache data
-      // from senpai.moe to learn the exact start time for a show (rather than this terrible estimate)
     }
     if (anime[animeAiringStatus] == 2) {
       // Completed anime; can just assume all episodes are out
@@ -64,4 +69,29 @@ function addAiredEpisodeCount(data) {
       anime[airedEpisodesKey] = 0;
     }
   }
+}
+
+const senpaiApiUrl = 'http://www.senpai.moe/export.php?type=json&src=raw'
+
+const malIdKey = 'MALID'
+const airdateKey = 'airdate_u'
+
+function loadAnimeStartTimes() {
+  var map = startTimeMap;
+  request(senpaiApiUrl, function(error, response, html) {
+    var jsonObject, malId, airdate;
+    if (!error) {
+      jsonObject = JSON.parse(html);
+      jsonObject.items.forEach((x) => {
+	malId = x[malIdKey];
+	airdate = x[airdateKey];
+	console.log(x.name);
+
+	map[malId] = airdate;
+      });
+      console.log("\nLoaded start times from Senpai.moe");
+    } else {
+      console.log(error);
+    }
+  });
 }
